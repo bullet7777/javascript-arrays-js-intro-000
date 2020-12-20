ebbb1c81bd21561fc217d8618e3fc59a9b16a1a8
Chris <chris.mahfouz@hotmail.com>
2020-12-20 15:02:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,47 +1,37 @@
-
-var chocolateBars = ['snickers', 'hundred grand','kitkat','skittles']
-
+var chocolateBars=['snickers','hundred grand','kitkat','skittles']
 function addElementToBeginningOfArray(array,element){
-var arr=[element,...array]
-
-return arr;
+  var array=[element,...array]
+  return array
 }
 function destructivelyAddElementToBeginningOfArray(array,element){
   array.unshift(element)
-  return array;
-}
-function accessElementInArray(array,index){
-
-  return array[index]
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.shift()
-  return array
-}
-function removeElementFromBeginningOfArray(array){
-  array.slice(1)
-  return array
-}
-function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop()
-  return array
-}
-function removeElementFromEndOfArray(array){
-  array.slice(0,array.length-1)
   return array
 }
 function addElementToEndOfArray(array,element){
-var arr=[...array,element]
-  return arr
-}
-function  destructivelyAddElementToEndOfArray(array, element){
-  array.push(element)
+  var array=[...array,element]
   return array
 }
-function removeElementFromBeginningOfArray(array){
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
 
+}
+function accessElementInArray(array,index){
+  var array=array[index]
+  return array
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+
+}
+function removeElementFromBeginningOfArray(array){
   return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+array.pop()
+  return array
+
 }
 function removeElementFromEndOfArray(array){
   return array.slice(0,array.length-1)
